socool728/pdi-git-plugin

commit 93b394b2f3e948be0c241facbfe2fb73c9e97998

Set MERGE_MSG as a commit message when merging

src/main/java/org/pentaho/di/git/spoon/model/UIGit.java
@@ -159,8 +159,16 @@ public class UIGit extends VCS implements IVCS {
    */
   @Override
   public String getCommitMessage( String commitId ) {
-    RevCommit commit = resolve( commitId );
-    return commit.getFullMessage();
+    if ( commitId.equals( IVCS.WORKINGTREE ) ) {
+      try {
+        return git.getRepository().readMergeCommitMsg();
+      } catch ( Exception e ) {
+        return e.getMessage();
+      }
+    } else {
+      RevCommit commit = resolve( commitId );
+      return commit.getFullMessage();
+    }
   }
 
   /* (non-Javadoc)

src/main/java/org/pentaho/di/git/spoon/GitController.java
@@ -475,7 +475,7 @@ public class GitController extends AbstractXulEventHandler {
       commitButton.setDisabled( false );
 
       setAuthorName( vcs.getAuthorName() );
-      setCommitMessage( "" );
+      setCommitMessage( vcs.getCommitMessage( IVCS.WORKINGTREE ) );
 
       changedFiles.addAll( vcs.getUnstagedFiles() );
       changedFiles.addAll( vcs.getStagedFiles() );

commit 747f4b123a967b4c40f5aa525e1c0bd680610311

Fix a bug that the commit message is not cleared (regression from 93b394b2)

src/main/java/org/pentaho/di/git/spoon/model/UIGit.java
@@ -158,7 +158,8 @@ public class UIGit extends VCS implements IVCS {
   public String getCommitMessage( String commitId ) {
     if ( commitId.equals( IVCS.WORKINGTREE ) ) {
       try {
-        return git.getRepository().readMergeCommitMsg();
+        String merge_msg = git.getRepository().readMergeCommitMsg();
+        return merge_msg == null ? "" : merge_msg;
       } catch ( Exception e ) {
         return e.getMessage();
       }